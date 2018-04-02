8643c6dd27a57aa09297ed9f891e6993d782a756
Elizeu Santos de Freitas <elizeu.freitas@outlook.com>
2018-04-01 23:36:19 -0300

    Aula 2.14

---
+++ src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -0,0 +1,30 @@
+package com.algaworks.cobranca;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+import org.thymeleaf.TemplateEngine;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@SpringBootApplication
+public class CobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobrancaApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+	
+	@Bean
+	public LayoutDialect layoutDialect() {
+	    return new LayoutDialect();
+	}
+}
