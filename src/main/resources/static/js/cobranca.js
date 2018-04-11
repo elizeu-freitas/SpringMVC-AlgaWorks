4e207a79011b41175ce51e45580e0fd5334669f1
Elizeu Santos de Freitas <elizeu.freitas@outlook.com>
2018-04-10 22:55:47 -0300

    Aula 3.1

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -13,3 +13,7 @@ $("#confirmacaoExclusaoModal").on('show.bs.modal', function(event){
 	
 	modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <strong>' + descricaoTitulo + '</strong> ?')
 });
+
+$(function(){
+	$('[rel="tooltip"]').tooltip()
+})
